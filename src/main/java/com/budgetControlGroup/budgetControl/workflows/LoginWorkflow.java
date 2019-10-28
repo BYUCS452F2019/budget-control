97408eb532e7c18b19a59b500c2692bd2ce50571
Jake <Jacob.m.livingston@gmail.com>
2019-10-28 12:28:52 -0600

    Login Workflow

--- src/main/java/com/budgetControlGroup/budgetControl/workflows/LoginWorkflow.java
+++ src/main/java/com/budgetControlGroup/budgetControl/workflows/LoginWorkflow.java
@@ -1,9 +1,18 @@
 package com.budgetControlGroup.budgetControl.workflows;
 
+import com.budgetControlGroup.budgetControl.database.PostgresConnection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginWorkflow {
+  private PostgresConnection postgresConnection;
+
+  @Autowired
+  public LoginWorkflow(PostgresConnection postgresConnection) {
+    this.postgresConnection = postgresConnection;
+  }
+
   public String login(String username, String password) {
     //check if user exists
     //if it doesnt, return empty string
