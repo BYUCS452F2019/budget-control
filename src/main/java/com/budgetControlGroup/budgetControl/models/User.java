a955ac9cf604d02503c5ed01addb6e96a6347a65
Jake <Jacob.m.livingston@gmail.com>
2019-10-28 12:28:38 -0600

    User Model

--- src/main/java/com/budgetControlGroup/budgetControl/models/User.java
+++ src/main/java/com/budgetControlGroup/budgetControl/models/User.java
@@ -1,4 +1,41 @@
 package com.budgetControlGroup.budgetControl.models;
 
+import java.util.Date;
+
+
 public class User {
+  int userId;
+  String username;
+  String firstName;
+  String lastName;
+  String email;
+  Date lastLogin;
+  Date dateCreated;
+
+  public User() {
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public Date getLastLogin() {
+    return lastLogin;
+  }
 }
