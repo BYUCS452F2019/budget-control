6e548fd05507f0fcaecd821521311b60f8a6d7af
Tanner Norton <tanneranorton23@gmail.com>
2019-11-15 12:18:01 -0700

    my stuff is working

---
+++ src/main/java/com/budgetControlGroup/budgetControl/Models/Budget_Item.java
@@ -0,0 +1,4 @@
+package com.budgetControlGroup.budgetControl.Models;
+
+public class Budget_Item {
+}
---
+++ src/main/java/com/budgetControlGroup/budgetControl/Models/Category.java
@@ -0,0 +1,4 @@
+package com.budgetControlGroup.budgetControl.Models;
+
+public class Category {
+}
